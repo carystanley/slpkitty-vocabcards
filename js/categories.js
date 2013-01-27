68c3c5a5a434907cea8d30be99de7875a94cb10a
Cary Stanley <cary@yahoo-inc.com>
2013-01-27 09:54:32 -0800

    Streamline Config

--- js/categories.js
+++ js/categories.js
@@ -3,12 +3,12 @@ YUI.add('categories', function (Y, NAME) {
   Y.Categories = {
     food: new Y.ItemList({
       items: [
-        new Y.ItemModel({id: 'apple', label: 'Apple', image: 'imgs/apple.svg'}),
-        new Y.ItemModel({id: 'carrot', label: 'Carrot', image: 'imgs/carrot.svg'}),
-        new Y.ItemModel({id: 'milk', label: 'Milk', image: 'imgs/milk.svg'}),
-        new Y.ItemModel({id: 'bread', label: 'Bread', image: 'imgs/bread.svg'}),
-        new Y.ItemModel({id: 'banana', label: 'Banana', image: 'imgs/banana.svg'}),
-        new Y.ItemModel({id: 'orange', label: 'Orange', image: 'imgs/orange.svg'})
+        {id: 'apple', label: 'Apple', image: 'imgs/apple.svg'},
+        {id: 'carrot', label: 'Carrot', image: 'imgs/carrot.svg'},
+        {id: 'milk', label: 'Milk', image: 'imgs/milk.svg'},
+        {id: 'bread', label: 'Bread', image: 'imgs/bread.svg'},
+        {id: 'banana', label: 'Banana', image: 'imgs/banana.svg'},
+        {id: 'orange', label: 'Orange', image: 'imgs/orange.svg'}
       ]
     })
   };
