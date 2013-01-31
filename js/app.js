39881513329c637e909938b3e5308d5dc8d2a82d
carystanley <cary@example.com>
2013-01-31 13:46:57 -0800

    Refactor the Base App to be more modular

--- js/app.js
+++ js/app.js
@@ -1,36 +1,50 @@
-YUI().use('app-base', 'item-model', 'item-modellist', 'exercise-model', 'settings-view', 'exercise-view',  'categories', function (Y) {
+YUI.add('speech-app', function (Y, NAME) {
+  Y.SpeechApp = Y.Base.create('speech-app', Y.App, [], {
 
-  var app = new Y.App({
-    container    : '#wrapper',
-    viewContainer: '#wrapper',
     views: {
       settings: {type: 'SettingsView', preserve: true},
       exercise: {type: 'ExerciseView', preserve: false}
+    },
+
+    initializer: function () {
+	  this.on('*:start', this.startExercise);
+    },
+
+    startExercise: function(e) {
+      this.set('exercise', new Y.ExerciseModel({
+        count: e.count,
+          items: Y.Categories[e.category]
+        }));
+      this.navigate('/slpkitty-vocabcards/exercise/');
+    },
+
+    showExercise: function (req) {
+      var self = this
+          exercise = this.get('exercise');
+
+      exercise.on('finished', function() {
+        self.navigate('/slpkitty-vocabcards/');
+      });
+      this.showView('exercise', {name: name, model: exercise})
+    },
+
+    showSettings: function () {
+      this.showView('settings');
+    }
+
+  }, {
+    ATTRS: {
+      routes: {
+        value: [
+          {path: '/slpkitty-vocabcards/', callbacks: 'showSettings'},
+          {path: '/slpkitty-vocabcards/exercise/', callbacks: 'showExercise'}
+        ]
+      }
     }
   });
 
-  app.route('/slpkitty-vocabcards/', function () {
-    this.showView('settings');
-  });
-
-  app.route('/slpkitty-vocabcards/exercise/', function (req) {
-    var self = this
-        exercise = this.get('exercise');
-
-    exercise.on('finished', function() {
-      self.navigate('/slpkitty-vocabcards/');
-    });
-    this.showView('exercise', {name: name, model: exercise})
-  });
-
-  app.on('*:start', function(e) {
-    this.set('exercise', new Y.ExerciseModel({
-      count: e.count,
-      items: Y.Categories[e.category]
-    }));
-    this.navigate('/slpkitty-vocabcards/exercise/');
-  });
-
-  app.render().dispatch().navigate('/slpkitty-vocabcards/');
-
+}, '0.0.2', {
+    requires : [
+        'app-base', 'item-model', 'item-modellist', 'exercise-model', 'settings-view', 'exercise-view',  'categories'
+    ]
 });
